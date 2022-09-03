c93b6fe47f468380af54d4133970953bdd220a73
Pablo Cruz-Morales <68575424+pablo-genomes-to-vials-cruz@users.noreply.github.com>
2022-09-03 13:43:16 +0200

    Update genbankidstolabel.pl

--- genbankidstolabel.pl
+++ genbankidstolabel.pl
@@ -1,9 +1,9 @@
-open FILE, $ARGV[0] or die "I cant read the core_seq.txt input file\n";
-#----------making fasta oneline-------
+open FILE, $ARGV[0] or die "I cant read the input file\n";
+
 while ($line=<FILE>){
 	if ($line=~/>/){
-	$line=~/(>)(..\_\d+\.\d+)(.+)\s\[(.+)\]/;
-	$label="$1$2_$4";
+	$line=~/(>.+\d+\..)\s(.+)\s\[(.+)\]/;
+	$label="$1_$3";
 	$label=~s/\s/_/g;
 	print "$label\n";	
 	}
